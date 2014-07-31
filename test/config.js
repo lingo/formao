c5e1c47b55d3e8bb5e6b67ab116ca22df6517bda
Luke Hudson <lukeletters@gmail.com>
2014-08-01 01:05:55 +0200

    Completed first set of unit tests, fixing bugs

---
+++ test/config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Sequelize = require('sequelize-sqlite').sequelize,
+    sequelize = new Sequelize('database', 'username', 'password', {
+        dialect: 'sqlite',
+        storage: 'testdb.sqlite',
+        logging: false
+    });
+
+var MyModel   = sequelize.define('MyModel', {
+    name:        { type: Sequelize.STRING,   allowNull: false },
+    description: { type: Sequelize.TEXT,     allowNull: true },
+    startDate:   { type: Sequelize.DATE,     allowNull: false, defaultValue: Sequelize.NOW },
+    endDate:     { type: Sequelize.DATE,     allowNull: false, defaultValue: Sequelize.NOW },
+    completed:   { type: Sequelize.BOOLEAN,  allowNull: false, defaultValue: false }
+});
+
+var express = require('express');
+var app     = express();
+var path    = require('path');
+
+app.set('views', path.resolve(__dirname, '../views'));
+app.set('view engine', 'jade');
+
+module.exports = {
+    app:    app,
+    models: {MyModel: MyModel},
+    db:     sequelize
+};
--- test/index.js
+++ test/index.js
@@ -1,47 +1,104 @@
-
-
 'use strict';
 /**
- * Testing of ???
+ * Testing of Formao
  */
 process.env.NODE_ENV = 'test';
 
 var should  = require('should');
+var cheerio = require('cheerio');
+var tidy    = require('htmltidy').tidy;
 
-var Sequelize = require('sequelize');
-
-var Sequelize = require('sequelize-sqlite').sequelize,
-    sequelize = new Sequelize('database', 'username', 'password', {
-        dialect: 'sqlite',
-        storage: 'testdb.sqlite',
-        logging: false
-    });
-
-
-var MyModel   = sequelize.define('MyModel', {
-  name:        { type: Sequelize.STRING,   allowNull: false },
-  description: { type: Sequelize.TEXT,     allowNull: true },
-  startDate:   { type: Sequelize.DATE,     allowNull: false, defaultValue: Sequelize.NOW },
-  endDate:     { type: Sequelize.DATE,     allowNull: false, defaultValue: Sequelize.NOW },
-  completed:   { type: Sequelize.BOOLEAN,  allowNull: false, defaultValue: false }
-});
+var config = require('./config');
+var Formao = require('../index').Formao;
 
 describe('It should render form correctly', function() {
+    // console.log(config.models.MyModel.rawAttributes);
+
+    var form        = null;
+    var fakeRequest = {
+        url: '/testaction',
+        body: {
+            name:        'Test name',
+            description: 'Test description',
+            startDate:   new Date('2014-08-01 00:32'),
+            endDate:     new Date('2014-08-30 00:32'),
+            completed:   false
+        }
+    };
+
     // this.timeout(0);
 
     beforeEach(function(done) {
+        form = null;
+        form = new Formao(config.models.MyModel);
         done();
     });
-    before(function(done) {
-        done();
+    // before(function(done) {
+    //     done();
+    // });
+
+    it('should set action', function() {
+        form.action('/project');
+        should.equal(form.action(), '/project');
     });
 
-    it('should test something', function(done) {
-        var Formao = require('../utils/forms').reForm;
-        var form = new Formao(database.models.Project);
+    it('should set method', function() {
+        form.method('GET');
+        form.method().should.equal('GET');
+    });
+
+    it('should chain set action and method', function() {
         form
-            .action('/project');
-        done(new Error("NOT IMPLEMENTED"));
+            .action('/project')
+            .method('GET');
+        form.method().should.equal('GET');
+        form.action().should.equal('/project');
+    });
+
+    it('should generate ID and be able to override', function() {
+        form.attr().should.have.property('id', 'MyModel_Form');
+        form.addAttribute('id', '123');
+        form.attr().should.have.property('id', '123');
+    });
+
+    it('should add single class', function() {
+        form.addClass('test');
+        form.attr().should.have.property('class', ['test']);
+    });
+
+    it('should add more classes', function() {
+        form
+            .addClass('test')
+            .addClass('another test');
+        form.attr().should.have.property('class', ['test','another']);
+    });
+
+    it('should render correctly', function(done) {
+        form
+            .addClass('mytest')
+            .addClass('verbose')
+            .addAttribute('data-testing', '12345')
+            .method('PUT')
+            .action(fakeRequest.url)
+            .fill(fakeRequest.body)
+            .render(config.app, fakeRequest)
+            .then(function(html) {
+                tidy(html, {indent: true}, console.log);
+                var $    = cheerio.load(html);
+                var $form = $('form#MyModel_Form');
+                should.exist($form);
+                $form.attr('data-testing').should.equal('12345');
+                $form.attr('method').should.equal('PUT');
+                $form.attr('action').should.equal(fakeRequest.url);
+                $form.attr('class').should.equal('mytest verbose');
+                $form.find('input').should.have.length(4);
+                $form.find('textarea').should.have.length(1);
+                $form.find('textarea').text().should.equal(fakeRequest.body.description);
+                $form.find('input.date').should.have.length(2);
+                $form.find('input#MyModel_Form_name').val().should.equal(fakeRequest.body.name);
+                done();
+            })
+            .catch(done);
     });
 });
 
